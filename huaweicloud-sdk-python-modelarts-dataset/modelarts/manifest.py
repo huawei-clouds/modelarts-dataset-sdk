c89accee5411d2fcd20a7bcd6507ebebf49469ae
xubo245 <xubo29@huawei.com>
2019-06-29 11:11:03 +0800

    support get annotations

--- huaweicloud-sdk-python-modelarts-dataset/modelarts/manifest.py
+++ huaweicloud-sdk-python-modelarts-dataset/modelarts/manifest.py
@@ -133,6 +133,35 @@ def getSources(manifest_path, source_type, obs_client=None):
   return sources
 
 
+def getAnnotations(value):
+  text = json.loads(value)
+  return __getAnnotationsInternal(text)
+
+
+def __getAnnotationsInternal(text):
+  annotations = text.get(field_name.annotation)
+  annotations_list = []
+  if annotations is not None:
+    for annotation in annotations:
+      annotation_type = annotation.get(field_name.annotation_type)
+      annotation_name = annotation.get(field_name.annotation_name)
+      annotation_loc = annotation.get(field_name.annotation_loc) or annotation.get(field_name.annotation_loc2)
+      annotation_creation_time = annotation.get(field_name.annotation_creation_time) or annotation.get(
+        field_name.annotation_creation_time2)
+      annotation_property = annotation.get(field_name.annotation_property)
+      annotation_format = annotation.get(field_name.annotation_format) or annotation.get(
+        field_name.annotation_format2)
+      annotation_confidence = annotation.get(field_name.annotation_confidence)
+      annotated_by = annotation.get(field_name.annotation_annotated_by) or annotation.get(
+        field_name.annotation_annotated_by2)
+      annotations_list.append(
+        Annotation(name=annotation_name, type=annotation_type, loc=annotation_loc,
+                   property=annotation_property,
+                   confidence=annotation_confidence,
+                   creation_time=annotation_creation_time,
+                   annotated_by=annotated_by, annotation_format=annotation_format))
+  return annotations_list
+
 def __getDataSet(lines):
   sample_list = []
   size = 0
@@ -146,28 +175,8 @@ def __getDataSet(lines):
       source_type = text.get(field_name.source_type)
       source_property = text.get(field_name.source_property)
       id = text.get(field_name.id)
-      annotations = text.get(field_name.annotation)
       inference_loc = text.get(field_name.inference_loc) or text.get(field_name.inference_loc2)
-      annotations_list = []
-      if annotations is not None:
-        for annotation in annotations:
-          annotation_type = annotation.get(field_name.annotation_type)
-          annotation_name = annotation.get(field_name.annotation_name)
-          annotation_loc = annotation.get(field_name.annotation_loc) or annotation.get(field_name.annotation_loc2)
-          annotation_creation_time = annotation.get(field_name.annotation_creation_time) or annotation.get(
-            field_name.annotation_creation_time2)
-          annotation_property = annotation.get(field_name.annotation_property)
-          annotation_format = annotation.get(field_name.annotation_format) or annotation.get(
-            field_name.annotation_format2)
-          annotation_confidence = annotation.get(field_name.annotation_confidence)
-          annotated_by = annotation.get(field_name.annotation_annotated_by) or annotation.get(
-            field_name.annotation_annotated_by2)
-          annotations_list.append(
-            Annotation(name=annotation_name, type=annotation_type, loc=annotation_loc,
-                       property=annotation_property,
-                       confidence=annotation_confidence,
-                       creation_time=annotation_creation_time,
-                       annotated_by=annotated_by, annotation_format=annotation_format))
+      annotations_list = __getAnnotationsInternal(text)
       sample_list.append(
         Sample(source=source, usage=usage, annotations=annotations_list, inference_loc=inference_loc, id=id,
                source_type=source_type, source_property=source_property))
---
+++ huaweicloud-sdk-python-modelarts-dataset/modelarts/test/test_getAnnotations.py
@@ -0,0 +1,34 @@
+# Copyright 2018 Deep Learning Service of Huawei Cloud. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import sys
+
+from modelarts import manifest
+
+def main(argv):
+    line = "{\"annotation\":[{\"name\":\"cat\",\"type\":\"modelarts/image_classification\",\"creation-time\":\"2019-06-28 16:07:52\",\"annotated-by\":\"human/ei_modelarts_y00218826_01/ei_modelarts_y00218826_01\"}],\"usage\":\"train\",\"source\":\"s3://obs-lzy/train_data/data05/data0509/data4/input/猫/CAT_04/00000990_004.jpg\",\"id\":\"0001842d9eb2a3078d835f0d18cf58c1\"}"
+    annotation_list = manifest.getAnnotations(line)
+    assert (1==len(annotation_list))
+    for annotation in annotation_list:
+      assert "cat" == annotation.get_name()
+      assert "modelarts/image_classification" == annotation.get_type()
+      assert "2019-06-28 16:07:52" == annotation.get_creation_time()
+      assert "human/ei_modelarts_y00218826_01/ei_modelarts_y00218826_01" == annotation.get_annotated_by()
+
+
+if __name__ == '__main__':
+  # If user want to test OBS, please input OBS path, ak, sk and endpoint.
+  main(sys.argv)
+  print("Success")
